7f7554c12ac4314a1fd267a2bb6bfc0fa6c9201c
Apple0726 <bpm_apple@hotmail.com>
2020-02-16 17:45:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("d")
+import arcade
+
+arcade.open_window(1920, 1080, "Helixteus 3")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+arcade.draw_circle_filled(900, 500, 50, arcade.color.BLACK)
+arcade.finish_render()
+arcade.run()
