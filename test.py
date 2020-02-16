092b57a4503a18201eb1bd925c21736ffa54ef3c
cheffplayer <dakrav51@gmail.com>
2020-02-16 09:09:11 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import arcade
 
+
+
 arcade.open_window(1920, 1080, "Helixteus 3")
-arcade.set_background_color(arcade.color.WHITE)
+
+arcade.set_background_color(arcade.color.BLIZZARD_BLUE)
+
 arcade.start_render()
-arcade.draw_circle_filled(900, 500, 50, arcade.color.BLACK)
+
+arcade.draw_circle_filled(900, 500, 60, arcade.color.GREEN)
+
 arcade.finish_render()
+
 arcade.run()
