b44806cf9ae19ecc76a152d59d155bbe9d2661d6
cortisol <hugi.nicholas@hotmail.com>
2020-02-18 00:57:01 +0800

    tiles no longer a seperate instances but are handled by a single instance "TileContainer", fixed zooming artefact, highlighting mouse overed tile

--- pygame_tests/planet_view.py
+++ pygame_tests/planet_view.py
@@ -1,43 +1,57 @@
+import math
+
 import pygame
 import pygame_gui
 
 
 tile_png = pygame.image.load('img/tile.png')
 
-
-class Tile(pygame.sprite.Sprite):
+class TileContainer:
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = tile_png
-        self.rect = self.image.get_rect()
+        self.scale = 2
         self.x = 0
         self.y = 0
-        self.scale = 1
+        self.width = 5
+        self.height = 5
+        self.hover = (0, 0)
 
-    def set_position(self, x, y):
-        self.x = x
-        self.y = y
+    def append(self, sprite):
+        self.tiles.append(sprite)
 
-    def update(self, *args):
-        super().update(args)
-        self.rect[0] = self.x * self.scale
-        self.rect[1] = self.y * self.scale
-        self.image = pygame.transform.scale(tile_png, (int(64 * self.scale), int(64 * self.scale)))
+    def update(self):
+        for tile in self.tiles:
+            tile.update()
 
+    def check_hover(self, mx, my):
+        # convert to local mouse pos
+        mx = (mx - self.x) / self.scale
+        my = (my - self.y) / self.scale
+        self.hover = (mx//64, my//64)
 
+    def draw(self, screen):
+        for x in range(self.width):
+            for y in range(self.height):
+                image = pygame.transform.scale(tile_png, (math.ceil(64 * self.scale), math.ceil(64 * self.scale)))
+                screen.blit(image, (self.x + x*64*self.scale, self.y + y*64*self.scale))
+
+        if 0 <= self.hover[0] < self.width and 0 <= self.hover[1] < self.height:
+            hover_x = 64 * self.hover[0] * self.scale + self.x
+            hover_y = 64 * self.hover[1] * self.scale + self.y
+            hover_size = 64 * self.scale
+            hover_surface = pygame.Surface((hover_size, hover_size), pygame.SRCALPHA)
+            hover_surface.fill((255, 255, 255, 100))
+            screen.blit(hover_surface, (hover_x, hover_y))
 
 
 class PlanetView:
     def __init__(self):
         self.manager = pygame_gui.UIManager((1280, 720))
         self.mouse_down = False
-        self.tiles = pygame.sprite.Group()
+        self.tiles = TileContainer()
 
-        for x in range(5):
-            for y in range(5):
-                tile = Tile()
-                tile.set_position(x*64, y*64)
-                self.tiles.add(tile)
+        self.zoom = 1
+        self.tiles.x = (1280 - 64*5)/2
+        self.tiles.y = (720 - 64*5)/2
 
     def process_event(self, event):
         self.manager.process_events(event)
@@ -49,30 +63,26 @@ class PlanetView:
             if event.button == 1:
                 self.mouse_down = False
             if event.button == 4:  # SCROLL UP
-                self.zoom_tiles(0.1)
+                self.zoom += 0.1
             if event.button == 5:  # SCROLL DOWN
-                self.zoom_tiles(-0.1)
+                self.zoom -= 0.1
         if event.type == pygame.MOUSEMOTION:
             if self.mouse_down:
                 self.pan_tiles(event.rel[0], event.rel[1])
+            self.tiles.check_hover(event.pos[0], event.pos[1])
 
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    def zoom_tiles(self, zoom):
-        for tile in self.tiles:
-            tile.scale += zoom
-
     def pan_tiles(self, dx, dy):
-        for tile in self.tiles:
-            tile.x += dx
-            tile.y += dy
+        self.tiles.x += dx
+        self.tiles.y += dy
 
     def update(self, delta):
         self.manager.update(delta)
 
-        self.tiles.update()
+        self.tiles.scale = self.tiles.scale-(self.tiles.scale-self.zoom)*delta*2
 
     def draw(self, screen):
         screen.fill((0, 0, 20))
---
+++ pygame_tests/scene.py
@@ -0,0 +1,2 @@
+# Base class for all scenes, not implemented yet
+
